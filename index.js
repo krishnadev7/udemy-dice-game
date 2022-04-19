16e60b06220a288658e7514c6da568aa1731c5c4
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2022-04-19 12:11:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+console.log(randomNumber1);
+console.log(randomNumber2);
+document.querySelector('.dice .img1').setAttribute('src', `/images/dice${randomNumber1}.png`);
+document.querySelector('.dice .img2').setAttribute('src', `/images/dice${randomNumber2}.png`);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector('h1').innerText = "Player 1 Wins! 🚩"
+}else if(randomNumber1===randomNumber2){
+     document.querySelector('h1').innerText = 'Draw ! 👍';
+}
+else{
+    document.querySelector('h1').innerText = 'Player 2 Wins! 🚩';
+}
+
+
